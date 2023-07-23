cccfc075455f1488139e32e5c44861b27f580a5e
CLancet <2385366128@qq.com>
2023-07-23 16:28:51 +0800

    update ignore file

--- v1.0/product-client/src/main/java/tongji/product/client/ClientStarter.java
+++ v1.0/product-client/src/main/java/tongji/product/client/ClientStarter.java
@@ -2,6 +2,7 @@ package tongji.product.client;
 import com.hundsun.jrescloud.common.boot.CloudApplication;
 import com.hundsun.jrescloud.common.boot.CloudBootstrap;
 
+@CloudApplication
 public class ClientStarter {
     public static void main(String[] args){
         CloudBootstrap.run(ClientStarter.class, args);
