8776f0935e11743b8516d145c52a9a565f098281
CyberObservers <13816750292@163.com>
2023-07-30 16:16:38 +0800

    Update SettlementDTO.java

--- v1.0/product-api/src/main/java/tongji/product/api/pojo/SettlementDTO.java
+++ v1.0/product-api/src/main/java/tongji/product/api/pojo/SettlementDTO.java
@@ -1,5 +1,8 @@
 package tongji.product.api.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Calendar;
 import java.util.Date;
 
@@ -8,6 +11,9 @@ public class SettlementDTO {
     // (ptr+1) %3 : 前天
     // (ptr+2) %3 : 昨天
     private int ptr;
+
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date[] dates;
 
     public SettlementDTO(){
